35d3036fa6ba42b3df0cb4b1fe95af03a81cce8d
Leila <leila.boukella@gmail.com>
2022-08-25 16:14:19 -0700

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter project name.',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Enter project description.',
+            name: 'desc',
+        },
+        {
+            type: 'input',
+            message: 'Enter installation instructions.',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Enter usage information.',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Enter test instructions.',
+            name: 'name',
+        },
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
